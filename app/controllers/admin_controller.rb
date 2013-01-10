6245d990fe3d3c2e74a1144aad57c786bfd657a0
Martijn Stegeman <martijn@stgm.nl>
2013-01-10 22:21:02 +0100

    show group counts, redirect after group import, skip empty lines

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -19,6 +19,7 @@ class AdminController < ApplicationController
 		# this is very dependent on datanose export format: id's in col 0 and 1, group name in 7
 		source = params[:paste]
 		source.each_line do |line|
+			next if line.strip == ""
 			line = line.split("\t")
 			group = Group.where(:name => line[7]).first_or_create if line[7] != ""
 			user = User.where('uvanetid in (?)', line[0..1]).first
@@ -30,6 +31,8 @@ class AdminController < ApplicationController
 				user.save
 			end
 		end
+		
+		redirect_to :back
 	end
 
 end
