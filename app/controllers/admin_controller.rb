47e7ed4ecca7b160e39d335e1d4109ce4f5e2cab
Martijn Stegeman <martijn@stgm.nl>
2013-01-10 22:16:50 +0100

    remove group link on empty group

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -16,6 +16,7 @@ class AdminController < ApplicationController
 	end
 	
 	def import_groups
+		# this is very dependent on datanose export format: id's in col 0 and 1, group name in 7
 		source = params[:paste]
 		source.each_line do |line|
 			line = line.split("\t")
@@ -23,6 +24,9 @@ class AdminController < ApplicationController
 			user = User.where('uvanetid in (?)', line[0..1]).first
 			if user && group
 				user.group = group
+				user.save
+			elsif user
+				user.group = nil
 				user.save
 			end
 		end
