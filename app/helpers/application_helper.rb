cc43f1fc3d7901995d1ba06f2db5c659579ee977
Daan Smit <d.smit@uva.nl>
2013-07-03 16:08:40 +0200

    fixed unreachable submit tabs by disabling auto_ids in kramdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,13 +13,14 @@ module ApplicationHelper
 	def markdown(text, page_context)
 		# pass public parl URL to the image converter
 		# :image_prefix is defined in config/application.rb
-		Kramdown::Document.new(text, 
-		                      :asset_prefix => page_context.public_url,
-							  :cdn_prefix => Course.cdn,
-		                      :coderay_css => :class,
-		                      :coderay_tab_width => 4,
-		                      :enable_coderay => true,
-		                      :coderay_line_numbers => nil).to_custom_html.html_safe
+		Kramdown::Document.new(text,
+		                       :auto_ids => false,
+		                       :asset_prefix => page_context.public_url,
+		                       :cdn_prefix => Course.cdn,
+		                       :coderay_css => :class,
+		                       :coderay_tab_width => 4,
+		                       :enable_coderay => true,
+		                       :coderay_line_numbers => nil).to_custom_html.html_safe
 	end
 	
 	def title()
