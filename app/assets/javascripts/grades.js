c4b979d7e593a88e0558b71a3aa9230454cda9a9
Daan Smit <d.smit@uva.nl>
2013-09-18 14:41:35 +0200

    added pass/fail button

---
+++ app/assets/javascripts/grades.js
@@ -0,0 +1,30 @@
+var GRADE_PASS = -1;
+var GRADE_FAIL =  0;
+
+function deactivate_buttons() {
+	$('button#pass-btn').removeClass('active');
+	$('button#fail-btn').removeClass('active');
+}
+
+function activate_button(button) {
+	deactivate_buttons();
+	button.addClass('active');
+}
+
+$(document).ready(function() {
+	$('button#pass-btn').click(function(e) {
+		$('#grade_grade').val(GRADE_PASS);
+		activate_button($(this));
+		e.preventDefault();
+	});
+
+	$('button#fail-btn').click(function(e) {
+		$('#grade_grade').val(GRADE_FAIL);
+		activate_button($(this));
+		e.preventDefault();
+	});
+
+	$('#grade_grade').change(function(e) {
+		deactivate_buttons();
+	});
+});
