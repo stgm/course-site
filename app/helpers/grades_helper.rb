8ed23f94b0396f10007f4c26eafd9956fb0925e4
Martijn Stegeman <martijn@stgm.nl>
2013-09-18 19:48:53 +0200

    grades

--- app/helpers/grades_helper.rb
+++ app/helpers/grades_helper.rb
@@ -1,2 +1,10 @@
 module GradesHelper
+	
+	def translate_grade(grade)
+		return "unknown" if grade < -1 or grade > 10
+		return "pass" if grade == -1
+		return "fail" if grade == 0
+		return grade.to_s
+	end
+	
 end
