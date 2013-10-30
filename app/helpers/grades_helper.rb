32b154edbfa9ce1461789afbbe3fe40173455c0b
Martijn Stegeman <martijn@stgm.nl>
2013-10-30 18:35:21 +0100

    display error value if grade is erroneous

--- app/helpers/grades_helper.rb
+++ app/helpers/grades_helper.rb
@@ -1,7 +1,7 @@
 module GradesHelper
 	
 	def translate_grade(grade)
-		return "unknown" if grade < -1 or grade > 10
+		return "error" if grade.nil? or grade < -1 or grade > 10
 		return "pass" if grade == -1
 		return "fail" if grade == 0
 		return grade.to_s
