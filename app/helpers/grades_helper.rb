e40284a6c0e467fe67b78f7abfe0e9878c1cf48f
Martijn Stegeman <martijn@stgm.nl>
2014-01-09 22:51:18 +0100

    show grades /10 if higher than 10

--- app/helpers/grades_helper.rb
+++ app/helpers/grades_helper.rb
@@ -1,9 +1,10 @@
 module GradesHelper
 	
 	def translate_grade(grade)
-		return "error" if grade.nil? or grade < -1 or grade > 10
+		return "error" if grade.nil? or grade < -1
 		return "pass" if grade == -1
 		return "fail" if grade == 0
+		return grade/10.0 if grade > 10
 		return grade.to_s
 	end
 	
