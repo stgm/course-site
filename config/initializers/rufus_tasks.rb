be31587397905d408d18eefb118e593506297a24
Martijn Stegeman <martijn@stgm.nl>
2014-03-15 10:13:45 +0100

    change mailing frequency

--- config/initializers/rufus_tasks.rb
+++ config/initializers/rufus_tasks.rb
@@ -1,9 +1,13 @@
 scheduler = Rufus::Scheduler.new
 
-scheduler.every '1m' do
-	Rails.logger.debug "Rufuuuuuus"
+# If you want to change the mailing frequency, note that this frequency is
+# present in two places in this code. One for running the scheduler regularly,
+# and one for making sure only grades of a certain age are emailed, to allow
+# for corrections within that timeframe.
+
+scheduler.every '1h' do
 	if Settings.send_grade_mails
-		Grade.where("updated_at < ? and mailed_at < updated_at and grade is not null", 1.minutes.ago).find_each do |g|
+		Grade.where("updated_at < ? and mailed_at < updated_at and grade is not null", 2.hours.ago).find_each do |g|
 			GradeMailer.new_mail(g).deliver
 			g.touch(:mailed_at)
 		end
