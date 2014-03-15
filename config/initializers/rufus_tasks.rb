631421bdf6d2746aff0af912114e647693ccb4f2
Martijn Stegeman <martijn@stgm.nl>
2014-03-15 09:27:45 +0100

    fix touching

--- config/initializers/rufus_tasks.rb
+++ config/initializers/rufus_tasks.rb
@@ -1,11 +1,11 @@
 scheduler = Rufus::Scheduler.new
 
-scheduler.every '15m' do
+scheduler.every '1m' do
 	Rails.logger.debug "Rufuuuuuus"
 	if Settings.send_grade_mails
-		Grade.where("updated_at < ? and mailed_at < updated_at and grade is not null", 15.minutes.ago).find_each do |g|
+		Grade.where("updated_at < ? and mailed_at < updated_at and grade is not null", 1.minutes.ago).find_each do |g|
 			GradeMailer.new_mail(g).deliver
-			g.update_attribute(:mailed_at, DateTime.now)
+			g.touch(:mailed_at)
 		end
 	end
 end
