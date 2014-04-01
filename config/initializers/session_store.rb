0088acfb4d5c0df6dbce8c447e57c6e47a719e76
Martijn Stegeman <martijn@stgm.nl>
2014-04-01 15:56:41 +0200

    allow cookie to expire later

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-CourseSite::Application.config.session_store :cookie_store, :key => '_course-site_session'
+CourseSite::Application.config.session_store :cookie_store, :key => '_course-site_session', expire_after: 2.months
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
