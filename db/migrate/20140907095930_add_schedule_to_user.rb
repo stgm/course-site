95f9c10cc4ea974f04ab54e649a4b27fb2558084
Martijn Stegeman <martijn@stgm.nl>
2014-09-07 12:30:16 +0200

    ok

--- db/migrate/20140907095930_add_schedule_to_user.rb
+++ db/migrate/20140907095930_add_schedule_to_user.rb
@@ -4,15 +4,5 @@ class AddScheduleToUser < ActiveRecord::Migration
 		add_column :users, :status, :string
 		add_reference :users, :schedule, index: true
 		add_reference :users, :schedule_span, index: true
-		
-		User.all.each do |u|
-			if r = u.registrations.first
-				u.term = r.term
-				u.status = r.status
-				u.schedule_id = r.schedule_id
-				u.schedule_span_id = r.schedule_span_id
-				u.save
-			end
-		end
 	end
 end
