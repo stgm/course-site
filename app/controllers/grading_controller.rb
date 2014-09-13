51f955bfe22f40a5b09d6b2a7725bb789311a16d
Martijn Stegeman <martijn@stgm.nl>
2014-09-13 21:06:23 +0200

    allow filtering on grading page

--- app/controllers/grading_controller.rb
+++ app/controllers/grading_controller.rb
@@ -7,7 +7,12 @@ class GradingController < ApplicationController
 	# List of problems to be graded by assistants
 	#
 	def index
-		@submits = Submit.includes(:user, :pset, :grade).where("grades.updated_at < submits.updated_at or grades.updated_at is null or grades.updated_at > ?", 1.days.ago).order('psets.name')
+		@submits = Submit.includes(:user, :pset, :grade).where("grades.updated_at < submits.updated_at or grades.updated_at is null or grades.updated_at > ?", 1.days.ago)
+		@submits = @submits.where(pset_id:params[:pset]) if not params[:pset].blank?
+		@submits = @submits.where("users.group_id" => params[:group]) if not params[:group].blank?
+		@submits = @submits.order('psets.name')
+		@groups = Group.all
+		@psets = Pset.all
 	end
 	
 	private
