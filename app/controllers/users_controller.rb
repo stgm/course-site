468940f0ce3a208ab54ab6086d7d34ff67b4f5af
Martijn Stegeman <martijn@stgm.nl>
2015-07-09 11:18:57 +0200

    remove class-level respond

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,10 @@ class UsersController < ApplicationController
 	before_filter CASClient::Frameworks::Rails::Filter
 	before_filter :require_admin
 
-	respond_to :json
-
 	def update
 		p = User.find(params[:id])
 		p.update_attributes!(params[:user])
-		respond_with p
+		render json: p
 	end
 	
 end
