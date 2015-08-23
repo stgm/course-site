0161177a138306d3fdaf9b198c1b7227842f79df
Martijn Stegeman <martijn@stgm.nl>
2015-08-23 12:09:33 +0200

    multi-login logins

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150729182552) do
+ActiveRecord::Schema.define(version: 20150823084359) do
 
   create_table "alerts", force: :cascade do |t|
     t.string   "title"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20150729182552) do
   add_index "answers", ["user_id"], name: "index_answers_on_user_id"
 
   create_table "categories", force: :cascade do |t|
-    t.string   "title"
+    t.string   "title",      limit: 255
     t.integer  "position"
     t.integer  "subpage_id"
     t.datetime "created_at"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20150729182552) do
 
   create_table "grades", force: :cascade do |t|
     t.integer  "submit_id"
-    t.string   "grader"
+    t.string   "grader",      limit: 255
     t.integer  "scope"
     t.integer  "correctness"
     t.integer  "design"
@@ -50,26 +50,33 @@ ActiveRecord::Schema.define(version: 20150729182552) do
     t.integer  "grade"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.datetime "mailed_at",   default: '-4712-01-01 00:00:00', null: false
+    t.datetime "mailed_at",               default: '-4712-01-01 00:00:00', null: false
   end
 
   add_index "grades", ["submit_id"], name: "index_grades_on_submit_id"
 
   create_table "groups", force: :cascade do |t|
-    t.string   "name"
+    t.string   "name",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
+  create_table "logins", force: :cascade do |t|
+    t.string  "login"
+    t.integer "user_id"
+  end
+
+  add_index "logins", ["user_id"], name: "index_logins_on_user_id"
+
   create_table "pages", force: :cascade do |t|
-    t.string   "title"
+    t.string   "title",      limit: 255
     t.integer  "position"
     t.integer  "section_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "slug"
-    t.string   "path"
-    t.boolean  "public",     default: false
+    t.string   "slug",       limit: 255
+    t.string   "path",       limit: 255
+    t.boolean  "public",                 default: false
   end
 
   add_index "pages", ["slug", "section_id"], name: "index_pages_on_slug_and_section_id", unique: true
@@ -88,7 +95,7 @@ ActiveRecord::Schema.define(version: 20150729182552) do
   add_index "pings", ["user_id"], name: "index_pings_on_user_id"
 
   create_table "pset_files", force: :cascade do |t|
-    t.string   "filename"
+    t.string   "filename",   limit: 255
     t.boolean  "required"
     t.integer  "pset_id"
     t.datetime "created_at"
@@ -98,7 +105,7 @@ ActiveRecord::Schema.define(version: 20150729182552) do
   add_index "pset_files", ["pset_id"], name: "index_pset_files_on_pset_id"
 
   create_table "psets", force: :cascade do |t|
-    t.string   "name"
+    t.string   "name",        limit: 255
     t.text     "description"
     t.integer  "page_id"
     t.datetime "created_at"
@@ -124,8 +131,8 @@ ActiveRecord::Schema.define(version: 20150729182552) do
   create_table "registrations", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "track_id"
-    t.string   "term"
-    t.string   "status"
+    t.string   "term",             limit: 255
+    t.string   "status",           limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "schedule_id"
@@ -136,7 +143,7 @@ ActiveRecord::Schema.define(version: 20150729182552) do
   add_index "registrations", ["user_id"], name: "index_registrations_on_user_id"
 
   create_table "schedule_spans", force: :cascade do |t|
-    t.string   "name"
+    t.string   "name",        limit: 255
     t.integer  "schedule_id"
     t.text     "content"
     t.datetime "created_at"
@@ -146,7 +153,7 @@ ActiveRecord::Schema.define(version: 20150729182552) do
   add_index "schedule_spans", ["schedule_id"], name: "index_schedule_spans_on_schedule_id"
 
   create_table "schedules", force: :cascade do |t|
-    t.string   "name"
+    t.string   "name",        limit: 255
     t.integer  "track_id"
     t.text     "description"
     t.datetime "created_at"
@@ -156,18 +163,18 @@ ActiveRecord::Schema.define(version: 20150729182552) do
   add_index "schedules", ["track_id"], name: "index_schedules_on_track_id"
 
   create_table "sections", force: :cascade do |t|
-    t.string   "title"
+    t.string   "title",      limit: 255
     t.integer  "position"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "slug"
-    t.string   "path"
+    t.string   "slug",       limit: 255
+    t.string   "path",       limit: 255
   end
 
   add_index "sections", ["slug"], name: "index_sections_on_slug", unique: true
 
   create_table "settings", force: :cascade do |t|
-    t.string   "var",                   null: false
+    t.string   "var",        limit: 255, null: false
     t.text     "value"
     t.integer  "thing_id"
     t.string   "thing_type", limit: 30
@@ -183,48 +190,46 @@ ActiveRecord::Schema.define(version: 20150729182552) do
     t.datetime "submitted_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "url"
+    t.string   "url",          limit: 255
   end
 
   add_index "submits", ["pset_id"], name: "index_submits_on_pset_id"
   add_index "submits", ["user_id"], name: "index_submits_on_user_id"
 
   create_table "subpages", force: :cascade do |t|
-    t.string   "title"
+    t.string   "title",      limit: 255
     t.text     "content"
     t.integer  "position"
     t.integer  "page_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "slug"
+    t.string   "slug",       limit: 255
   end
 
   add_index "subpages", ["slug"], name: "index_subpages_on_slug", unique: true
 
   create_table "tracks", force: :cascade do |t|
     t.integer  "final_grade_id"
-    t.string   "name"
+    t.string   "name",           limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "tracks", ["final_grade_id"], name: "index_tracks_on_final_grade_id"
-
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "uvanetid"
-    t.string   "mail"
-    t.string   "avatar"
+    t.string   "name",             limit: 255
+    t.string   "uvanetid",         limit: 255
+    t.string   "mail",             limit: 255
+    t.string   "avatar",           limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "group_id"
-    t.boolean  "done",             default: false
-    t.boolean  "active",           default: true
-    t.string   "term"
-    t.string   "status"
+    t.boolean  "done",                         default: false
+    t.boolean  "active",                       default: true
+    t.string   "term",             limit: 255
+    t.string   "status",           limit: 255
     t.integer  "schedule_id"
     t.integer  "schedule_span_id"
-    t.string   "token"
+    t.string   "token",            limit: 255
   end
 
   add_index "users", ["schedule_id"], name: "index_users_on_schedule_id"
