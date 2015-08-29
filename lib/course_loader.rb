f1dfff4fadc4133c4e9dec346a6b0cb633d91cd4
Martijn Stegeman <martijn@stgm.nl>
2015-08-29 23:53:48 +0200

    prune

--- lib/course_loader.rb
+++ lib/course_loader.rb
@@ -181,8 +181,7 @@ private
 						db_pset.page = db_page  # restore link to owning page!
 						db_pset.save
 						
-						# dangerous, will delete pset that has submits
-						# Pset.where("id != ?", db_pset).where(page_id: db_page).delete_all
+						Pset.where("id != ?", db_pset).where(page_id: db_page).update_all(page_id: nil)
 
 						# remove previous files
 						db_pset.pset_files.delete_all
