cd0eed56d104ac87803ba31fe73fef001e50ffe4
Martijn Stegeman <martijn@stgm.nl>
2015-09-10 22:01:01 +0200

    colors

--- app/helpers/grading_helper.rb
+++ app/helpers/grading_helper.rb
@@ -2,7 +2,8 @@ module GradingHelper
 	
 	def grade_color(grade)
 		return '' if grade.nil?
-		return 'success' if grade.done
+		return 'success' if grade['public']
+		return 'warning' if grade.done
 		return 'info'
 	end
 	
