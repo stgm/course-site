40173e6a722b2982e4456e7b6759809333d4c7cc
Martijn Stegeman <martijn@stgm.nl>
2015-09-17 11:51:16 +0200

    comma

--- app/mailers/grade_mailer.rb
+++ app/mailers/grade_mailer.rb
@@ -10,7 +10,7 @@ class GradeMailer < ActionMailer::Base
 		@feedback = grade.comments
 		@grade = grade.grade
 		Rails.logger.info ENV["MAILER_ADDRESS"]
-		mail(to: grade.user.mail, bcc: 'tim.doolan@gmail.com' subject: "Feedback for #{Settings.short_course_name}")
+		mail(to: grade.user.mail, bcc: 'tim.doolan@gmail.com', subject: "Feedback for #{Settings.short_course_name}")
 	end
 	
 end
