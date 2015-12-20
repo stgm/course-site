f1ac0ad56f10c6a88cf4ca6d79afd0653964e5f6
Martijn Stegeman <martijn@stgm.nl>
2015-12-20 14:59:18 +0100

    allow grade mail template

--- app/mailers/grade_mailer.rb
+++ app/mailers/grade_mailer.rb
@@ -9,6 +9,7 @@ class GradeMailer < ActionMailer::Base
 		@grade_name = grade.pset.name
 		@feedback = grade.comments
 		@grade = grade.grade
+		@header = File.read("#{Rails.root}/public/course/mail/grade.txt") if File.exists?("#{Rails.root}/public/course/mail/grade.txt")
 		Rails.logger.info ENV["MAILER_ADDRESS"]
 		mail(to: grade.user.mail, subject: "Feedback for #{Settings.short_course_name}")
 	end
