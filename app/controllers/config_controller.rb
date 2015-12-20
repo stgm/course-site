2ebb0f41067d81e88e0f53b25d4aff30e3f191f5
Martijn Stegeman <martijn@stgm.nl>
2015-12-20 20:09:56 +0100

    allow branch config and cloning/pulling

--- app/controllers/config_controller.rb
+++ app/controllers/config_controller.rb
@@ -29,9 +29,14 @@ class ConfigController < ApplicationController
 	end
 	
 	def git_repo_save
-		Settings.git_repo = params[:repo_url]
-		CourseLoader.new.run
-		redirect_to :back, notice: 'The course content was successfully cloned.'
+		if Settings.git_repo.present?
+			redirect_to :back, alert: 'You already cloned a repo once!'
+		else
+			Settings.git_repo = params[:repo_url]
+			Settings.git_branch = params[:repo_branch]
+			CourseLoader.new.run
+			redirect_to :back, notice: 'The course content was successfully cloned.'
+		end
 	end
 	
 	def generate_secret
--- lib/course_git.rb
+++ lib/course_git.rb
@@ -1,24 +1,29 @@
+#
+#  Get remote git data, either by pulling existing, or cloning anew.
+#
 module CourseGit
 
 	def self.pull
-		if git = self.local_repo
+		# allow configuration of remote branch, with default
+		remote_branch = Settings.git_branch || 'master'
+
+		if git = self.existing_local_repo
 			begin
-				git.pull # 'origin', 'hispeed' no support for branches...
+				git.pull 'origin', git.current_branch
 			rescue Git::GitExecuteError
 				return false
 			end
 		else
 			if Settings.git_repo.present?
-				git = Git.clone(Settings.git_repo, 'public/course', depth:1, log:Rails.logger)
+				git = Git.clone(Settings.git_repo, 'public/course', branch:remote_branch, depth:1, log:Rails.logger)
 			end
 		end
 		
 		return true
 	end
 	
-	def self.local_repo
-		git = Git.open('public/course', log:Rails.logger)
-		return git
+	def self.existing_local_repo
+		return Git.open('public/course', log:Rails.logger)
 	rescue
 		return nil
 	end
