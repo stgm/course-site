33c0093e4153b41652912806ff653469689fd5f5
Martijn Stegeman <martijn@stgm.nl>
2016-02-09 19:18:26 +0100

    only e-mail handmade grades

--- app/mailers/grade_mailer.rb
+++ app/mailers/grade_mailer.rb
@@ -8,7 +8,7 @@ class GradeMailer < ActionMailer::Base
 		@course_name = Settings.short_course_name
 		@grade_name = grade.pset.name
 		@feedback = grade.comments
-		@grade = grade.any_final_grade
+		@grade = grade.grade
 		@login = grade.submit.used_login if grade.submit
 		@header = File.read("#{Rails.root}/public/course/mail/grade.txt") if File.exists?("#{Rails.root}/public/course/mail/grade.txt")
 		Rails.logger.info ENV["MAILER_ADDRESS"]
