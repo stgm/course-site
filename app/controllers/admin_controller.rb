afca414161dd4a025df72efbeba33b67d95155b1
Martijn Stegeman <martijn@stgm.nl>
2016-08-27 21:48:16 +0200

    regd

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -33,10 +33,10 @@ class AdminController < ApplicationController
 	end
 	
 	def stats
+		@geregistreerd = User.count
 		@gestart = User.joins(:submits).uniq.count
 		final = Pset.find_by_name('final')
 		@gehaald = User.joins(:grades => :submit).where('submits.pset_id = ?', final).uniq.count
-		@terms = User.select("distinct term")
 		render layout: false
 	end
 		
