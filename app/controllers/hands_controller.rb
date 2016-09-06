1c42bd5c2f728bc8be5e3c2b56fb44dab1b66030
Martijn Stegeman <martijn@stgm.nl>
2016-09-06 15:53:11 +0200

    order

--- app/controllers/hands_controller.rb
+++ app/controllers/hands_controller.rb
@@ -4,8 +4,8 @@ class HandsController < ApplicationController
 	before_filter :require_admin_or_assistant
 
 	def index
-		@my_hands = Hand.where(done:false, assist:current_user).order('created_at desc')
-		@hands = Hand.where(done:false, assist:nil).order('created_at desc')
+		@my_hands = Hand.where(done:false, assist:current_user).order('created_at asc')
+		@hands = Hand.where(done:false, assist:nil).order('created_at asc')
 		@long_time_users = User.not_admin.where('last_seen_at > ? and (last_spoken_at < ? or last_spoken_at is null)', 25.minutes.ago, 2.days.ago).order('updated_at desc')
 	end
 	
