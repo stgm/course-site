290725295fbf984ee27cc731dba5094815ae55b6
Martijn Stegeman <martijn@stgm.nl>
2016-10-06 10:30:30 +0200

    no more mail forms

--- app/controllers/ask_controller.rb
+++ app/controllers/ask_controller.rb
@@ -4,39 +4,12 @@ class AskController < ApplicationController
 
 	def do
 		if params[:how]
-			if params[:how] == 'email'
-				helpscout = HelpScout::Client.new(ENV['HELPSCOUT_API_KEY'])
-				helpscout.create_conversation(
-				{
-					type: 'email',
-					customer: {
-						email: current_user.mail
-					},
-					subject: "New question from #{Settings.short_course_name}",
-					mailbox: {
-						id: 19905
-					},
-					tags: ['live'],
-					threads: [
-						{
-							type: 'customer',
-							createdBy: {
-								email: current_user.mail,
-								type: 'customer'
-							},
-							body: params['question']
-						}
-					]
-				})
-				# AskMailer.ask_me_anything(current_user, params['question'], request.remote_ip).deliver_later
-				# flash[:notice] = "Your question has been received! Expect an answer sometime soon."
-			elsif params[:how] = 'hands'
+			if params[:how] = 'hands'
 				hand = Hand.create(user:current_user, help_question:params[:question], location:params[:location])
 				if Features.slack_integration?
 					notifier = Slack::Notifier.new ENV['SLACK_WEBHOOK'], channel: Settings.hands_slack_channel
 					notifier.ping "*#{hand.user.name}* needs help at *#{hand.location}* <#{hands_url}|dibs!>\n#{hand.help_question}"
 				end
-				# flash[:notice] = "Your question has been received! Expect someone to arrive soon."
 			end
 		end
 
