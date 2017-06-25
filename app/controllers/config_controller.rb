d9abd89145541a264b717251cb4021928548b5e5
Martijn Stegeman <martijn@stgm.nl>
2017-06-25 15:03:32 +0200

    simplify settings page

--- app/controllers/config_controller.rb
+++ app/controllers/config_controller.rb
@@ -5,11 +5,11 @@ class ConfigController < ApplicationController
 	before_filter CASClient::Frameworks::Rails::Filter
 	before_filter :require_admin
 	
-	# key entry page, also shows if already having a session
-	def dropbox
+	def index
 		@dropbox_linked = Dropbox.connected?
+		@secret = Settings.webhook_secret
 	end
-	
+
 	def git_repo_save
 		if Settings.git_repo.present?
 			redirect_to :back, alert: 'You already cloned a repo once!'
@@ -25,10 +25,6 @@ class ConfigController < ApplicationController
 		secret = SecureRandom.hex(20)
 		Settings.webhook_secret = secret
 		render js:"$('#secret').html('#{secret}');"
-	end
-	
-	def webhook
-		@secret = Settings.webhook_secret
 	end
 
 end
