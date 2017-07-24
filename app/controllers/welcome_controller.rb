36e1072c05668458d8b19ad33ffd605ecf91a404
Martijn Stegeman <martijn@stgm.nl>
2017-07-24 19:08:02 +0200

    onboarding

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,20 +4,20 @@ class WelcomeController < ApplicationController
 
 	# welcome#index allows claiming of website
 	def index
-		
-		#redirect_to :root if authenticated?
-		
 		if not authenticated?
 			# Please login
 		elsif not logged_in?
+			# Please register
 			redirect_to profile_path and return
 		else
-			redirect_to admin_path and return
-		end
-		
-		if logged_in?
-			unless User.where(role: User.roles['admin']).count > 0
-				current_user.admin!
+			# Please admin all the things
+			if logged_in?
+				unless User.where(role: User.roles['admin']).count > 0
+					current_user.admin!
+				end
+			end
+			if current_user.admin?
+				redirect_to admin_path and return
 			end
 		end
 	end
