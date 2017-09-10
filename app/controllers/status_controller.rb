b2910ae7993cc381e192065f4a35a94d7cbfa59d
Martijn Stegeman <martijn@stgm.nl>
2017-09-10 13:26:53 +0200

    status updates

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -7,7 +7,7 @@ class StatusController < ApplicationController
 		if current_user.admin?
 			scope = Hand
 		else
-			scope = @current_schedule.schedule.hands
+			scope = current_user.schedule.hands
 		end
 		@updates = scope.where("note is not null and note != ''").order("updated_at desc")
 	end
