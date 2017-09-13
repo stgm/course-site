d5cee0a71f5138d22872f38ac5e0009097bfe9a5
Martijn Stegeman <martijn@stgm.nl>
2017-09-13 10:14:58 +0200

    small fix

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -6,7 +6,7 @@ class GradesController < ApplicationController
 	def show
 		@submit = Submit.find(params[:submit_id])
 		if @submit
-			if @submit.grade && (@submit.grade.published? || @submit.grade.discussed?)
+			if @submit.grade && (@submit.grade.finished? || @submit.grade.published? || @submit.grade.discussed?)
 				@grade = @submit.grade
 			else
 				form
