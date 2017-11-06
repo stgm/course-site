d72a54d4a4ebe2083df3a0feb09733cf5f8933f5
Martijn Stegeman <martijn@stgm.nl>
2017-11-06 18:59:18 +0100

    grouping

--- app/models/group.rb
+++ app/models/group.rb
@@ -24,8 +24,8 @@ class Group < ActiveRecord::Base
 	end
 	
 	def self.import(source)
-		User.update_all(group_id: nil)
-		Group.delete_all
+		# delete all groups that are not in use by assistants
+		Group.where.not(id: Group.joins(:users).where("users.id": User.staff)).delete_all
 	
 		source.each_line do |line|
 			next if line.strip == ""
@@ -47,71 +47,3 @@ class Group < ActiveRecord::Base
 	end
 
 end
-
-
-			#
-			# if user_id[0] == user_id[1]
-			# 	login = Login.where(login: user_id[0]).first_or_create
-			# 	user = login.user or (user = login.create_user and login.save)
-			# 	user.update_columns(name: user_name, mail: user_mail) if user.name.blank? or user.name =~ /,/
-			# 	if group_name != ""
-			# 		group = Group.where(:name => group_name).first_or_create
-			# 		user.group = group
-			# 		user.save
-			# 	else
-			# 		user.group = nil
-			# 		user.save
-			# 	end
-			# else
-			# 	first_user = User.with_login(user_id[0]).first
-			# 	second_user = User.with_login(user_id[1]).first
-			# 	if first_user.nil? && second_user.nil?
-			# 		login = Login.where(login: user_id[0]).first_or_create
-			# 		user = login.user or (user = login.create_user and login.save)
-			# 		login2 = Login.where(login: user_id[1]).first_or_create
-			# 		login2.user = login.user and login2.save
-			# 		user.update_columns(name: user_name, mail: user_mail) if user.name.blank? or user.name =~ /,/
-			# 		if group_name != ""
-			# 			group = Group.where(:name => group_name).first_or_create
-			# 			user.group = group
-			# 			user.save
-			# 		else
-			# 			user.group = nil
-			# 			user.save
-			# 		end
-			# 	elsif first_user == second_user
-			# 		user = first_user
-			# 		if group_name != ""
-			# 			group = Group.where(:name => group_name).first_or_create
-			# 			user.group = group
-			# 			user.save
-			# 		else
-			# 			user.group = nil
-			# 			user.save
-			# 		end
-			# 	elsif first_user.nil?
-			# 		login = Login.where(login: user_id[0]).first_or_create
-			# 		login.user = second_user and login.save
-			# 		user = second_user
-			# 		if group_name != ""
-			# 			group = Group.where(:name => group_name).first_or_create
-			# 			user.group = group
-			# 			user.save
-			# 		else
-			# 			user.group = nil
-			# 			user.save
-			# 		end
-			# 	elsif second_user.nil?
-			# 		login = Login.where(login: user_id[1]).first_or_create
-			# 		login.user = first_user and login.save
-			# 		user = first_user
-			# 		if group_name != ""
-			# 			group = Group.where(:name => group_name).first_or_create
-			# 			user.group = group
-			# 			user.save
-			# 		else
-			# 			user.group = nil
-			# 			user.save
-			# 		end
-			# 	end
-			# end
