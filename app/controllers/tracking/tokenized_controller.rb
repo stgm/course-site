54c19d7efb8bf914871a36de956e46687ffb1e17
Martijn Stegeman <martijn@stgm.nl>
2017-11-09 10:00:31 +0100

    update for the now

--- app/controllers/tracking/tokenized_controller.rb
+++ app/controllers/tracking/tokenized_controller.rb
@@ -1,11 +1,11 @@
 class Tracking::TokenizedController < ActionController::Base
 	
 	before_action :require_user
-	before_action :admin_rights?, only: [:list_students, :clear]
-	before_action :create_ping, only: [:ping, :gone, :help]
+	# before_action :admin_rights?, only: [:list_students, :clear]
+	before_action :create_ping, only: [:ping, :gone]
 	
 	def identify
-		if admin_rights?
+		if current_user.staff?
 			render json: { role: 'assistant' }
 		else
 			render json: { role: 'student' }
@@ -22,46 +22,42 @@ class Tracking::TokenizedController < ActionController::Base
 		render json: true
 	end
 	
-	def help
-		current_user.ping.help = !!params[:help]
-		current_user.ping.help_question = params[:help_question] if params[:help]
-		current_user.ping.save
-		render json: true
-	end
-	
-	def clear
-		hashid = Hashids.new(ENV['DROPBOX_KEY'])
-	
-		# make student #id 7 digit at least
-		id = "%07d" % hashid.decode(params[:user])
-
-		User.find_by_uvanetid(id).ping.update_attribute(:help, false)
-		User.find_by_uvanetid(id).ping.update_attribute(:help_question, nil)
-		render json: true
-	end
-	
-	def list_assistants
-		hashid = Hashids.new(ENV['DROPBOX_KEY'])
-	
-		result = Ping.active.assistants.map { |a| { id:hashid.encode(a.user.uvanetid.to_i), name:a.user.name, loca:a.loca, locb:a.locb, updated:a.updated_at } }
-		render json: result
-	end
-	
-	def list_students
-		hashid = Hashids.new(ENV['DROPBOX_KEY'])
-	
-		result = Ping.active.students.map { |a| { id:hashid.encode(a.user.uvanetid.to_i), name:a.user.name, loca:a.loca, locb:a.locb, help:!!a.help, help_question:a.help_question, updated:a.updated_at } }
-		render json: result
-	end
+	# def help
+	# 	current_user.ping.help = !!params[:help]
+	# 	current_user.ping.help_question = params[:help_question] if params[:help]
+	# 	current_user.ping.save
+	# 	render json: true
+	# end
+	#
+	# def clear
+	# 	hashid = Hashids.new(ENV['DROPBOX_KEY'])
+	#
+	# 	# make student #id 7 digit at least
+	# 	id = "%07d" % hashid.decode(params[:user])
+	#
+	# 	User.find_by_uvanetid(id).ping.update_attribute(:help, false)
+	# 	User.find_by_uvanetid(id).ping.update_attribute(:help_question, nil)
+	# 	render json: true
+	# end
+	#
+	# def list_assistants
+	# 	hashid = Hashids.new(ENV['DROPBOX_KEY'])
+	#
+	# 	result = Ping.active.assistants.map { |a| { id:hashid.encode(a.user.uvanetid.to_i), name:a.user.name, loca:a.loca, locb:a.locb, updated:a.updated_at } }
+	# 	render json: result
+	# end
+	#
+	# def list_students
+	# 	hashid = Hashids.new(ENV['DROPBOX_KEY'])
+	#
+	# 	result = Ping.active.students.map { |a| { id:hashid.encode(a.user.uvanetid.to_i), name:a.user.name, loca:a.loca, locb:a.locb, help:!!a.help, help_question:a.help_question, updated:a.updated_at } }
+	# 	render json: result
+	# end
 	
 	private
 	
 	def create_ping
 		current_user.create_ping if !current_user.ping
-	end
-	
-	def admin_rights?
-		current_user.admin? || current_user.assistant?
 	end
 	
 	def require_user
