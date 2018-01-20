cdba90a18849fe5b7999c79fd52d49d9b0e03b1d
Martijn Stegeman <martijn@stgm.nl>
2018-01-20 18:28:56 +0100

    hands

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -4,7 +4,7 @@ class StatsController < ApplicationController
 	before_filter :require_admin
 	
 	def hands
-		@today = Hand.includes(:assist).where("created_at > ?", DateTime.now.beginning_of_day).order("created_at desc")
+		@today = Hand.includes(:assist).where("created_at > ?", DateTime.yesterday.beginning_of_day).order("created_at desc")
 	end
 
 end
