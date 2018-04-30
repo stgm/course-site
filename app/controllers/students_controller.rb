5f1ccf2bb8031f3aec05057071bd15230118b8b9
Martijn Stegeman <martijn@stgm.nl>
2018-04-30 16:26:41 +0200

    allow deactivation

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -25,7 +25,7 @@ class StudentsController < ApplicationController
 		end
 		
 		@psets = Pset.order(:order)
-		@users = User.student.where({ schedule: @current_schedule }).includes([:group, { :submits => :grade }]).order("groups.name").order(:name)
+		@users = User.student.active.where({ schedule: @current_schedule }).includes([:group, { :submits => :grade }]).order("groups.name").order(:name)
 		@users = @users.group_by(&:group)
 	end
 	
@@ -35,7 +35,21 @@ class StudentsController < ApplicationController
 		
 	end
 	
+	# who is inactive but still registered for some schedule earlier?
 	def list_inactive
+		@psets = Pset.order(:order)
+		@schedules = Schedule.all
+		if @schedules.any?
+			@users = User.inactive.student.where("schedule_id is not null").order(:name).group_by(&:schedule)
+		else
+			@users = User.inactive.student.where("schedule_id is not null").order(:name).group_by(&:group)
+		end
+		@submits = Submit.includes(:grade).group_by(&:user_id)
+		render "index"
+	end
+	
+	# who didn't even start etc?
+	def list_other
 		@psets = Pset.order(:order)
 		@schedules = Schedule.all
 		@users = User.where('schedule_id' => nil).student.order(:name).group_by(&:group)
@@ -55,9 +69,10 @@ class StudentsController < ApplicationController
 	
 	def load_stats
 		@active_count = User.active.student.count
-		@inactive_count = User.student.where(schedule_id: nil).count
+		@inactive_count = User.student.inactive.where("schedule_id is not null").count
+		@other_count =  User.student.where(schedule_id: nil).count
 		@admin_count = User.staff.count
-		@schedule_count = User.student.group(:schedule_id).count
+		@schedule_count = User.student.active.group(:schedule_id).count
 		@title = 'List users'
 	end
 
