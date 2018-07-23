cc0697ca714d95f33c4a12f63e970a5f04adfdb1
Martijn Stegeman <martijn@stgm.nl>
2018-07-23 18:08:16 +0200

    rudimentaire timeline

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -29,7 +29,7 @@ class PageController < ApplicationController
 			@items = []
 			@items += @student.submits.where("submitted_at not null").to_a
 			@items += @grades.to_a
-			@items += @alerts.to_a
+			# @items += @alerts.to_a
 			# @items += @student.hands.to_a
 			# @items += @student.notes.to_a
 			@items = @items.sort { |a,b| b.created_at <=> a.created_at }
