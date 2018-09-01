29f7d7f9eda5247696320b4ea1cb3cfd56c0fdae
Martijn Stegeman <martijn@stgm.nl>
2018-09-01 18:13:41 +0200

    feedback

--- app/models/submit.rb
+++ app/models/submit.rb
@@ -6,6 +6,7 @@ class Submit < ActiveRecord::Base
 	has_one :grade, dependent: :destroy
 
 	serialize :submitted_files
+	serialize :check_feedback
 
 	def graded?
 		return (self.grade && (!self.grade.grade.blank? || !self.grade.calculated_grade.blank?))
@@ -14,7 +15,8 @@ class Submit < ActiveRecord::Base
 	def retrieve_feedback
 		path = File.join(Dropbox.root_folder, Settings.dropbox_folder_name, user.login_id, self.folder_name, 'check_feedback.json')
 		begin
-			contents = Dropbox.download(path)
+			json = Dropbox.download(path)
+			contents = json.present? ? JSON.parse(json) : nil
 			self.update(check_feedback: contents)
 		rescue
 			# go on, assuming its not there
@@ -27,6 +29,28 @@ class Submit < ActiveRecord::Base
 		# rescue
 		# 	# done anyway, assuming its not there
 		# end
+	end
+	
+	def check_feedback_formatted
+		return "" if self.check_feedback.blank?
+
+		result = ""
+		self.check_feedback.each do |item|
+			case item["status"]
+			when true
+				result << ":)"
+			when false
+				result << ":("
+			when nil
+				result << ":|"
+			end
+			result << " " + item["description"] + "\n"
+			if item["rationale"].present?
+				result << "    " + item["rationale"] + "\n"
+			end
+		end
+		
+		return result
 	end
 
 end
