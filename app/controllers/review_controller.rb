b6379cdf9eb7504dc6233565de2a3d267898d815
Martijn Stegeman <martijn@stgm.nl>
2018-09-09 15:39:31 +0200

    rudi review

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -6,7 +6,7 @@ class ReviewController < ApplicationController
 	layout "full-width"
 
 	def index
-		@users = current_user.schedule.users.includes(submits: [:grade, :pset]).where("grades.status in (?)", [Grade.statuses[:open], Grade.statuses[:finished]]).references(:grades)
+		@users = current_user.schedule.users.includes(submits: [:grade, :pset]).where("grades.status in (?)", [Grade.statuses[:open], Grade.statuses[:finished]]).references(:grades).order("grades.grader_id")
 	end
 	
 end
