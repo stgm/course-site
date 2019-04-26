287c235ab9a15d4958c8f042dd822ec2f5873e45
Martijn Stegeman <martijn@stgm.nl>
2019-04-26 10:46:08 +0200

    huh

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -59,7 +59,7 @@ class PageController < ApplicationController
 	end
 	
 	def syllabus
-		load_ann
+		# load_ann
 		# the normal homepage is the page without a parent section
 		@page = Page.where(:section_id => nil).first
 		@title = "#{Settings.course["short_name"]}  #{t(:syllabus)}"
