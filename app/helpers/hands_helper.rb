7c6295159d60d99bd27a7bb49881c8ecf8fcfc79
Martijn Stegeman <martijn@stgm.nl>
2019-09-07 10:43:14 +0200

    add colors for group names in hands list

---
+++ app/helpers/hands_helper.rb
@@ -0,0 +1,14 @@
+module HandsHelper
+
+	def get_colors_for_text(t)
+		background = Digest::SHA1.hexdigest(t).slice(0,6)
+		rgbval = background.hex
+		r = rgbval >> 16
+		g = (rgbval & 65280) >> 8
+		b = rgbval & 255
+		brightness = r*0.299 + g*0.587 + b*0.114
+		foreground = (brightness > 160) ? "000" : "fff"
+		return "color: \##{foreground}; background-color: \##{background}"
+	end
+
+end
