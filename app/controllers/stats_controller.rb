4cbb093313ebfe7fe17d71549b2cdec916b5bd28
Martijn Stegeman <martijn@stgm.nl>
2019-09-08 17:14:21 +0200

    change range

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -17,8 +17,8 @@ class StatsController < ApplicationController
 		# @week_data = Hand.where(done: true).where("updated_at > ?", 1.week.ago).select("strftime('%H',created_at) as the_hour, date(created_at) as the_date").map { |h| { x: h.the_date.remove("-"), y: h.the_hour.to_i+2, r: 1 } }
 		# @week_data =  Hand.where(done: true).where("updated_at > ?", 1.week.ago).group_by_hour(:created_at).count.map { |h| [ h.created_at, h.the_hour.to_i+2] }
 		
-		date1 = Date.today.at_beginning_of_week - 1.week
-		date2 = Date.today.at_beginning_of_week
+		date1 = 2.weeks.ago.beginning_of_day
+		date2 = 0.days.ago.beginning_of_day
 		@week_data =  Hand.where(done: true).where("updated_at > ? and updated_at < ?", date1, date2).group_by_hour(:created_at).count.map { |h,v| { x: h.day, y: h.hour.to_i+2, r: v } }
 
 		#@week_data = Hand.where(id: Schedule.find_by_name("Lospeed").hands, done: true).where("updated_at > ? and updated_at < ?", 8.weeks.ago, Date.today).group_by_day_of_week("created_at").count.map { |h,v| { x: "#{h.day_of_week}", y: h.hour.to_i+2, r: v } }
