b50d15081b746efe8229dd70dc7bc82d68ba7b2d
Martijn Stegeman <martijn@stgm.nl>
2019-09-18 17:12:10 +0200

    don't show autograded stuff to assist

--- app/models/submit.rb
+++ app/models/submit.rb
@@ -20,7 +20,7 @@ class Submit < ActiveRecord::Base
 		includes(:user, :pset, :grade).
 		where(grades: { status: [nil, Grade.statuses[:open]] }).
 		where(users: { active: true }).
-		where("psets.automatic = ? or not (psets.automatic = ? and submits.check_results is not null)", false, true).
+		where("psets.automatic = ? or submits.check_results is not null", false).
 		order('submits.created_at asc')
 	end
 
