fed4065bd74b2363f901aa7041000acbcf405c65
Martijn Stegeman <martijn@stgm.nl>
2019-11-12 16:21:25 +0100

    addon

--- app/models/submit.rb
+++ app/models/submit.rb
@@ -124,14 +124,23 @@ class Submit < ActiveRecord::Base
 	end
 	
 	def register_check_results(json)
+		# put results into db
 		self.check_token = nil
 		self.check_results = json
+		
+		# create a create if needed
 		grade = self.grade || self.build_grade
+		
+		# check via the grade if this submit is OK
 		grade.set_calculated_grade
+		grade.status = Grade.statuses[:published]
+		
+		self.save
+
+		# if not OK, send an e-mail
 		if grade.calculated_grade == 0
 			GradeMailer.bad_submit(self).deliver
 		end
-		self.save
 	end
 	
 	def check_feedback_problems?
