0bdd2bbbe6d84eaa099f887c553ebb86c44756a6
Martijn Stegeman <martijn@stgm.nl>
2020-01-25 18:45:45 +0100

    testing

--- app/helpers/progress_helper.rb
+++ app/helpers/progress_helper.rb
@@ -10,6 +10,15 @@ module ProgressHelper
 	
 	def progress_bar(user, items, **args)
 		@grouped_items = items.group_by_day{ |i| i.class==Submit && i.submitted_at || i.updated_at }
+		@xtra = user.submits.includes({:pset => [:parent_mod, :mod]}).where("submitted_at is not null").where("psets.mod_id is not null or mods_psets.pset_id is null").references(:psets, :mods).to_a.group_by_day{|i| i.created_at}
+		# @xtra.each { |k,v| puts k; puts v }
+		# puts "---"
+		
+		
+		@grouped_items.merge!(@xtra) { |k,v1,v2| puts k; puts v1; puts v2; v1 + v2 }
+		# puts @grouped_items
+		# @grouped_items.each { |k,v| puts k; puts v }
+		
 		tag.div class: "blockgraph #{args} #{args[:class]}" do
 			@attendance = user.attendance_by_day
 			@attendance.default = 0
