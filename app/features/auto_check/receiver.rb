fde3b9b87db60d47869c24b607f38cab56189137
Martijn Stegeman <martijn@stgm.nl>
2020-02-03 12:55:38 +0100

    assign first admin as grader for auto grades

--- app/features/auto_check/receiver.rb
+++ app/features/auto_check/receiver.rb
@@ -11,11 +11,9 @@ module AutoCheck::Receiver
 		grade = self.grade || self.build_grade
 		
 		# check via the grade if this submit is OK
-		self.automatic_scores.each do |k,v|
-			grade.subgrades[k] = v
-		end
 		grade.set_calculated_grade
 		grade.status = Grade.statuses[:published]
+		grade.grader = User.admin.first
 		grade.save
 		
 		self.save
