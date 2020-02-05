0b175ffb85857c875ec6cd652f8558dd27a946e4
Martijn Stegeman <martijn@stgm.nl>
2020-02-05 09:14:34 +0100

    fix receiving autogrades

--- app/features/auto_check/receiver.rb
+++ app/features/auto_check/receiver.rb
@@ -3,14 +3,19 @@ module AutoCheck::Receiver
 	extend ActiveSupport::Concern
 
 	def register_auto_check_results(json)
-		# put results into db
+		# save the raw results
 		self.check_token = nil
 		self.check_results = json
 		
 		# create a create if needed
 		grade = self.grade || self.build_grade
 		
-		# check via the grade if this submit is OK
+		# overwrite previous automatic scores
+		self.automatic_scores.each do |k,v|
+			grade.subgrades[k] = v
+		end
+
+		# immediately try calculating the grade and publishing
 		grade.set_calculated_grade
 		grade.status = Grade.statuses[:published]
 		grade.grader = User.admin.first
@@ -18,7 +23,7 @@ module AutoCheck::Receiver
 		
 		self.save
 
-		# if not OK, send an e-mail
+		# if the results do not appear OK, send an e-mail
 		if grade.calculated_grade == 0
 			GradeMailer.bad_submit(self).deliver
 		end
