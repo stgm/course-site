f316b1007428e9281f1c767b2e1e879fc5f14f0a
Martijn Stegeman <martijn@stgm.nl>
2020-04-15 11:23:42 +0200

    don't autopublish all the grades

--- app/features/auto_check/receiver.rb
+++ app/features/auto_check/receiver.rb
@@ -7,26 +7,28 @@ module AutoCheck::Receiver
 		self.check_token = nil
 		self.check_results = json
 		
-		# create a create if needed
-		grade = self.grade || self.build_grade
+		if self.pset.config['auto_publish']
+			# create a create if needed
+			grade = self.grade || self.build_grade
 		
-		# overwrite previous automatic scores
-		self.automatic_scores.each do |k,v|
-			grade.subgrades[k] = v
+			# overwrite previous automatic scores
+			self.automatic_scores.each do |k,v|
+				grade.subgrades[k] = v
+			end
+
+			# immediately try calculating the grade and publishing
+			grade.set_calculated_grade
+			grade.status = Grade.statuses[:published]
+			grade.grader = User.admin.first
+			grade.save
+		
+			# if the results do not appear OK, send an e-mail
+			if grade.calculated_grade == 0
+				GradeMailer.bad_submit(self).deliver
+			end
 		end
 
-		# immediately try calculating the grade and publishing
-		grade.set_calculated_grade
-		grade.status = Grade.statuses[:published]
-		grade.grader = User.admin.first
-		grade.save
-		
 		self.save
-
-		# if the results do not appear OK, send an e-mail
-		if grade.calculated_grade == 0
-			GradeMailer.bad_submit(self).deliver
-		end
 	end
 	
 end
