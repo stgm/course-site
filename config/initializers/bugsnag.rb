99d181d2023daaeaa5b511a9e766b3e81326415e
Martijn Stegeman <martijn@stgm.nl>
2020-06-06 16:48:30 +0200

    ignore notfounds

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -2,4 +2,5 @@ Bugsnag.configure do |config|
 	config.api_key = ENV['BUGSNAG_ID']
 	config.notify_release_stages = ['production']
 	config.ignore_classes << ActionController::RoutingError
+	config.ignore_classes << ActiveRecord::RecordNotFound
 end
