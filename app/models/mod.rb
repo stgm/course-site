fe904decaf879109b28fa308da6feaf82d97aae7
Martijn Stegeman <martijn@stgm.nl>
2020-07-01 17:19:36 +0200

    allow url links

--- app/models/mod.rb
+++ app/models/mod.rb
@@ -15,7 +15,7 @@ class Mod < ApplicationRecord
 		return nil if content.blank?
 		return content.map{|x| prepend_path(x,path)} if content.class==Array
 		return content.map{|k,v| [k,prepend_path(v,path)]}.to_h if content.class==Hash
-		return content.first=='/' ? content : File.join('/', path, content)
+		return content.first=='/' || content.include?(':') ? content : File.join('/', path, content)
 	end
 
 end
