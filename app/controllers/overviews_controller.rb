56f6a601414f9d841e434301f6ee9085dc1c7ed2
Martijn Stegeman <martijn@stgm.nl>
2020-09-04 08:34:21 +0200

    refactor

--- app/controllers/overviews_controller.rb
+++ app/controllers/overviews_controller.rb
@@ -31,48 +31,20 @@ class OverviewsController < ApplicationController
 		# [["Problems", ["M1", "M2", "M3", ...]], ...]
 		@overview = Settings.grading.select { |c,v| v['show_progress'] }.map { |c,v| [c, v['submits'].map {|k,v| k}] }
 		@overview = Settings.grading['modules'].to_a + @overview if Settings.grading['modules']
-
-		@name = @selected_schedule.name
-		@status = params[:status]
-		@psets = Pset.ordered_by_grading #Pset.order(:order)
-		@grouped_psets = @psets.group_by &:name
-
-		@users = @selected_schedule.users.not_staff.includes(:group, { submits: [:pset, :grade] }).order("groups.name").order(:name)
-		@title = 'List users'
-
-		@active_count = @users.active.count
-		@registered_count = @users.registered.count
-		@inactive_count = @users.inactive.count
-		@done_count = @users.done.count
-
-		case params[:status]
-		when 'active'
-			@users = @users.active
-		when 'registered'
-			@users = @users.registered
-		when 'inactive'
-			@users = @users.inactive
-		when 'done'
-			@users = @users.done
-		end
-
-		@users = @users.group_by(&:group)
-
-		if current_user.assistant?
-			render 'overview'
-		else
-			render 'show'
-		end
+		load_data
 	end
 
 	def schedule
 		# check which schedules this user is allowed to view
 		@accessible_schedules = current_user.accessible_schedules
 		@selected_schedule = Schedule.friendly.find(params[:slug])
+		load_data
+	end
 
+	def load_data
 		@name = @selected_schedule.name
 		@status = params[:status]
-		@psets = Pset.ordered_by_grading #Pset.order(:order)
+		@psets = Pset.ordered_by_grading
 		@grouped_psets = @psets.group_by &:name
 
 		@users = @selected_schedule.users.not_staff.includes(:group, { submits: [:pset, :grade] }).order("groups.name").order(:name)
@@ -95,12 +67,6 @@ class OverviewsController < ApplicationController
 		end
 
 		@users = @users.group_by(&:group)
-
-		if current_user.assistant?
-			render 'overview'
-		else
-			render 'show'
-		end
 	end
 
 end
