5c8158d3eb71dc19c71eac6a7f4ae7c06283ff43
Martijn Stegeman <martijn@stgm.nl>
2020-09-10 19:57:04 +0200

    cleanup for 2020

--- app/models/mod.rb
+++
@@ -1,6 +0,0 @@
-class Mod < ApplicationRecord
-
-	has_many :psets # that belong to the module
-	belongs_to :pset # to assign grades for this module
-
-end
--- app/models/pset.rb
+++ app/models/pset.rb
@@ -1,11 +1,9 @@
 class Pset < ApplicationRecord
 
 	belongs_to :page, optional: true
-	has_one :mod
-	belongs_to :parent_mod, class_name: "Mod", foreign_key: "mod_id", optional: true
-	delegate :pset, to: :parent_mod, prefix: 'parent', allow_nil: true
-	# belongs_to :mod
-	# has_one :parent_mod, class_name: "Mod"
+	
+	belongs_to :parent_pset, :class_name => 'Pset', optional: true
+	has_many :child_psets, :class_name => 'Pset', :foreign_key => 'parent_pset_id'
 
 	has_many :pset_files
 	has_many :submits
