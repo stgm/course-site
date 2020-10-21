c15efc3752aeaaeedaa697472deccbc568d008e1
Martijn Stegeman <martijn@stgm.nl>
2020-10-21 10:00:03 +0200

    fix null

--- app/models/user.rb
+++ app/models/user.rb
@@ -149,7 +149,7 @@ class User < ApplicationRecord
 	def hands_overview
 		hands.where(success:true).map do |h|
 			if h.closed_at.present? && h.claimed_at.present?
-				[h.id, h.claimed_at, (h.closed_at - h.claimed_at)/60, h.assist_name]
+				[h.id, h.claimed_at, (h.closed_at - h.claimed_at)/60, h.assist_name || ""]
 			end
 		end.compact
 	end
