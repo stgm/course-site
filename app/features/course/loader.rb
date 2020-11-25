eaa18c50c6352c299e10ed30dae875b56de17d4f
Martijn Stegeman <martijn@stgm.nl>
2020-11-25 15:17:54 +0100

    add warning when specifying grades that do not exist

--- app/features/course/loader.rb
+++ app/features/course/loader.rb
@@ -94,6 +94,23 @@ private
 
 		if grading = read_config(File.join(dir, 'grading.yml'))
 			Settings['grading'] = grading
+			validate_grading(grading)
+		end
+	end
+	
+	def validate_grading(grading_config)
+		progress_categories = grading_config.select { |category, value| value['show_progress'] }
+		if progress_categories.any?
+			if grading_config['grades'].blank?
+				@errors << "Problem loading grading.yml. There are grading categories like #{progress_categories.first.first} but no grades section is present specifying how to calculate grades."
+				return
+			end
+			all_submit_names = progress_categories.map { |k,v| [k,v['submits'].keys] }
+			invalid_grade_names = all_submit_names.map { |k,v| [k,v.select { |name| !grading_config['grades'].include?(name) }] }.select { |k,v| v.any? }.map{|k,v| "#{k}->#{v.join(',')}"}
+			if invalid_grade_names.any?
+				@errors << "Problem loading grading.yml. Some grades were specified as part of the final grade, but could not be found in the grades section: #{invalid_grade_names.join('; ')}."
+				return
+			end
 		end
 	end
 	
