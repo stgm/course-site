d451506f8365fee2ea4444514681f1998b51dd1c
Martijn Stegeman <martijn@stgm.nl>
2020-12-29 19:38:24 +0100

    more changes

--- app/helpers/modal_helper.rb
+++ app/helpers/modal_helper.rb
@@ -2,7 +2,6 @@ module ModalHelper
 
 	def link_to(name = nil, options = nil, html_options = {}, &block)
 		if html_options.include?(:target) && html_options[:target] != '_new'
-			logger.info("REBUILDING")
 			target = html_options[:target]
 			html_options.delete :target
 			html_options.deep_merge! ({ data: { 'turbo-frame' => target } })
