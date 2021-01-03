bab185fcdc39fe89f27c4a6624b716e19f3b779b
Martijn Stegeman <martijn@stgm.nl>
2021-01-03 16:43:50 +0100

    make user import more robust

--- app/controllers/schedules/import_groups_controller.rb
+++ app/controllers/schedules/import_groups_controller.rb
@@ -1,22 +1,26 @@
-#
-# import group assignments from a CSV paste
-#
+# Import group assignments from a CSV paste.
 class Schedules::ImportGroupsController < Schedules::ApplicationController
 
 	before_action :authorize
 	before_action :require_admin
-	
+
 	def new
 		load_schedule
 		@paste = Settings.cached_user_paste
 		render_to_modal header: 'Import groups'
 	end
-	
+
+	def propose
+		load_schedule
+		if @paste = params[:paste]
+			Settings.cached_user_paste = @paste
+			@result = @schedule.propose_groups(@paste)
+		end
+	end
+
 	def create
 		load_schedule
-		# this is very dependent on datanose export format: ids in col 0 and 1, group name in 7
 		if source = params[:paste]
-			Settings.cached_user_paste = source
 			@schedule.import_groups(source)
 		end
 		redirect_to schedule_overview_path(@schedule), notice: 'Student groups were successfully imported.'
