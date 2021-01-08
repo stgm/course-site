63e3e4c7e620b40ddda37a40ddca52cbbb0767fa
Martijn Stegeman <martijn@stgm.nl>
2021-01-08 17:50:57 +0100

    don't filter _blank targets for links

--- app/helpers/modal_helper.rb
+++ app/helpers/modal_helper.rb
@@ -1,10 +1,10 @@
 module ModalHelper
 
 	def link_to(name = nil, options = nil, html_options = {}, &block)
-		if html_options.include?(:target) && html_options[:target] != '_new'
+		if html_options.include?(:target) && !html_options[:target].in?(['_new', '_blank'])
 			target = html_options[:target]
 			html_options.delete :target
-			html_options.deep_merge! ({ data: { 'turbo-frame' => target } })
+			html_options.deep_merge! ({ data: { 'turbo-frame': target } })
 		end
 		super(name, options, html_options, &block)
 	end
