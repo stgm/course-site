c712b633bad7c16b870761887d3079c7da405a59
Martijn Stegeman <martijn@stgm.nl>
2021-01-23 17:56:03 +0100

    improve change logging and status editing

--- app/models/user/change_logger.rb
+++ app/models/user/change_logger.rb
@@ -8,9 +8,31 @@ module User::ChangeLogger
 	private
 
 	def log_changes
-		changes = self.previous_changes.select{|k,v| ['active', 'done', 'status','schedule_id','alarm'].include?(k)}
-		if changes.any?
-			self.notes.create(text: changes.collect{|k,v| "#{k.humanize} set to #{v[1]}  "}.join, author: Current.user, log: true)
+		changes = self.previous_changes.slice('active', 'done', 'status', 'schedule_id', 'alarm', 'group_id', 'role')
+
+		changes.each do |k,new_value|
+			text = case k
+				when 'active'
+					new_value[1] ? "Marked as active" : "Marked as inactive"
+				when 'done'
+					new_value[1] ? "Marked as done" : "Marked as not done"
+				when 'status'
+					new_value[1].present? ? "Status set to '#{new_value[1]}'" : "Status cleared"
+				when 'schedule_id'
+					"Schedule changed to #{self.schedule_name}"
+				when 'group_id'
+					"Group assignment changed to #{self.group_name}"
+				when 'alarm'
+					new_value[1] ? "Alarm set" : "Alarm unset"
+				when 'role'
+					"Activated as #{new_value[1]}"
+				end
+
+			self.notes.create(
+				text: text,
+				author: Current.user,
+				log: true
+			)
 		end
 	end
 end
