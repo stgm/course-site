c2d55bd2e3f539d08e57ce2d768deac92cdaf45a
Martijn Stegeman <martijn@stgm.nl>
2021-02-10 16:41:37 +0100

    order

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,9 +2,9 @@ class TodosController < ApplicationController
 
 	def watch_list
 		if current_user.admin?
-			@watch_list = User.watching
+			@watch_list = User.watching.order(:name)
 		else
-			@watch_list = current_user.students
+			@watch_list = current_user.students.order(:name)
 		end
 	end
 
