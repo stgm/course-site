a8f48fe89a72e187b2b92a00c74aabe0dc4073ea
Martijn Stegeman <martijn@stgm.nl>
2021-02-10 17:38:34 +0100

    fine grained permissions

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,10 +1,28 @@
 class TodosController < ApplicationController
 
+	before_action :set_user_scope
+
 	def watch_list
 		if current_user.admin?
-			@watch_list = User.watching.order(:name)
+			@watch_list = @user_scope.watching.order(:name)
+		elsif current_user.head?
+			@watch_list = @user_scope.watching.order(:name)
 		else
-			@watch_list = current_user.students.order(:name)
+			@watch_list = @user_scope.order(:name)
+		end
+	end
+
+	private
+
+	# limits user operations to the scope allowed for the current user
+	def set_user_scope
+		@user_scope = case current_user.role
+		when 'assistant'
+			current_user.students
+		when 'head'
+			current_user.schedule.students
+		when 'admin'
+			User.student
 		end
 	end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,13 @@ class UsersController < ApplicationController
 	before_action :authorize
 	before_action :require_senior, except: :show
 	before_action :require_staff, only: :show
-	
+	before_action :set_user_scope
+
 	layout 'modal'
 
 	def search
 		if params[:text] != ""
-			@results = User.includes(:logins).where("users.name like ? or logins.login like ?", "%#{params[:text]}%", "%#{params[:text]}%").references(:logins).limit(10).order(:name)
+			@results = @user_scope.includes(:logins).where("users.name like ? or logins.login like ?", "%#{params[:text]}%", "%#{params[:text]}%").references(:logins).limit(10).order(:name)
 		else
 			@results = []
 		end
@@ -19,11 +20,9 @@ class UsersController < ApplicationController
 	end
 
 	def show
-		head :forbidden and return if not current_user.admin? and not current_user.students.find(params[:id])
-		
-		@student = User.includes(:hands, :notes).find(params[:id])
+		@student = @user_scope.includes(:hands, :notes).find(params[:id])
 		@note = Note.new(student_id: @student.id)
-		
+
 		if current_user.senior?
 			@schedules = Schedule.all
 			@groups = @student.schedule && @student.schedule.groups.order(:name) || []
@@ -37,12 +36,12 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-		@student = User.find(params[:id])
-		
+		@student = @user_scope.find(params[:id])
+
 	end
 
 	def update
-		@user = User.find(params[:id])
+		@user = @user_scope.find(params[:id])
 		@user.update!(params.require(:user).permit(
 			:name,
 			:status,
@@ -62,9 +61,23 @@ class UsersController < ApplicationController
 	def calculate_final_grade
 		# feature has to be enabled by supplying a grading.yml
 		raise ActionController::RoutingError.new('Not Found') if not Grading::FinalGradeAssigner.available?
-		@user = User.find(params[:id])
+		@user = @user_scope.find(params[:id])
 		result = Grading::FinalGradeAssigner.assign_final_grade(@user, current_user, only: params[:grades])
 		redirect_to @user
+	end
+
+	private
+
+	# limits user operations to the scope allowed for the current user
+	def set_user_scope
+		@user_scope = case current_user.role
+		when 'assistant'
+			current_user.students
+		when 'head'
+			current_user.schedule.students
+		when 'admin'
+			User
+		end
 	end
 
 end
