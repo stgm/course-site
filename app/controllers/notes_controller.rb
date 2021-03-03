373dc2fe860a71c8238059ef91d9ba8230e9a171
Martijn Stegeman <martijn@stgm.nl>
2021-03-03 16:41:09 +0100

    cosmetic

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,48 +1,48 @@
 class NotesController < ApplicationController
-	before_action :authorize
-	before_action :require_staff
-	before_action :require_admin, only: :index
+    before_action :authorize
+    before_action :require_staff
+    before_action :require_admin, only: :index
 
-	before_action :set_note, only: [:show, :edit, :update]
-	before_action :check_permission, only: [:show, :edit, :update]
+    before_action :set_note, only: [:show, :edit, :update]
+    before_action :check_permission, only: [:show, :edit, :update]
 
-	def index
-		@notes = Note.includes(:student).where(log: false).order(created_at: :desc).limit(30)
-		render layout: 'navbar'
-	end
+    def index
+        @notes = Note.includes(:student).where(log: false).order(created_at: :desc).limit(30)
+        render layout: 'navbar'
+    end
 
-	def show
-	end
+    def show
+    end
 
-	def create
-		@note = Note.create(note_params.merge({ author_id: current_user.id }))
-		redirect_to user_path(@note.student)
-	end
+    def create
+        @note = Note.create(note_params.merge({ author_id: current_user.id }))
+        redirect_to user_path(@note.student)
+    end
 
-	def edit
-	end
+    def edit
+    end
 
-	def update
-		if @note.update(note_params)
-			redirect_to @note
-		else
-			render :edit
-		end
-	end
+    def update
+        if @note.update(note_params)
+            redirect_to @note
+        else
+            render :edit
+        end
+    end
 
-	private
+    private
 
-	def set_note
-		@note = Note.find(params[:id])
-	end
+    def set_note
+        @note = Note.find(params[:id])
+    end
 
-	def check_permission
-		current_user.admin? ||
-			@note.author == current_user ||
-			current_user.students.find(@note.student)
-	end
+    def check_permission
+        current_user.admin? ||
+        @note.author == current_user ||
+        current_user.students.find(@note.student)
+    end
 
-	def note_params
-		params.require(:note).permit(:text, :author_id, :student_id, :done, :assignee_id)
-	end
+    def note_params
+        params.require(:note).permit(:text, :author_id, :student_id, :done, :assignee_id)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,83 +1,88 @@
 # TODO factor out search
 class UsersController < ApplicationController
 
-	before_action :authorize
-	before_action :require_senior, except: :show
-	before_action :require_staff, only: :show
-	before_action :set_user_scope
+    before_action :authorize
+    before_action :require_senior, except: :show
+    before_action :require_staff, only: :show
+    before_action :set_user_scope
 
-	layout 'modal'
+    layout 'modal'
 
-	def search
-		if params[:text] != ""
-			@results = @user_scope.includes(:logins).where("users.name like ? or logins.login like ?", "%#{params[:text]}%", "%#{params[:text]}%").references(:logins).limit(10).order(:name)
-		else
-			@results = []
-		end
-		respond_to do |format|
-			format.js
-		end
-	end
+    def search
+        if params[:text] != ""
+            @results = @user_scope.
+                includes(:logins).
+                where("users.name like ? or logins.login like ?", "%#{params[:text]}%", "%#{params[:text]}%").
+                references(:logins).
+                limit(10).
+                order(:name)
+        else
+            @results = []
+        end
+        respond_to do |format|
+            format.js
+        end
+    end
 
-	def show
-		@student = @user_scope.includes(:hands, :notes).find(params[:id])
-		@note = Note.new(student_id: @student.id)
+    def show
+        @student = @user_scope.includes(:hands, :notes).find(params[:id])
+        @note = Note.new(student_id: @student.id)
 
-		if current_user.senior?
-			@schedules = Schedule.all
-			@groups = @student.schedule && @student.schedule.groups.order(:name) || []
-			@psets = Pset.ordered_by_grading
-			@attend = @student.attendance_records.group_by_day(:cutoff, format: "%a %d-%m").count
-			@items = @student.items(true)
-		else
-			@items = @student.notes.includes(:author).order(created_at: :desc)
-			render 'notes'
-		end
-	end
+        if current_user.senior?
+            @schedules = Schedule.all
+            @groups = @student.schedule && @student.schedule.groups.order(:name) || []
+            @psets = Pset.ordered_by_grading
+            @attend = @student.attendance_records.group_by_day(:cutoff, format: "%a %d-%m").count
+            @items = @student.items(true)
+        else
+            @items = @student.notes.includes(:author).order(created_at: :desc)
+            render 'notes'
+        end
+    end
 
-	def edit
-		@student = @user_scope.find(params[:id])
+    def edit
+        @student = @user_scope.find(params[:id])
 
-	end
+    end
 
-	def update
-		@user = @user_scope.find(params[:id])
-		@user.update!(params.require(:user).permit(
-			:name,
-			:status,
-			:alarm,
-			:status_description,
-			:mail,
-			:avatar,
-			:notes,
-			:schedule_id,
-			:group_id))
-		respond_to do |format|
-			format.js { head :ok }
-			format.html { redirect_to @user }
-		end
-	end
+    def update
+        @user = @user_scope.find(params[:id])
+        @user.update!(params.require(:user).permit(
+            :name,
+            :status,
+            :alarm,
+            :status_description,
+            :mail,
+            :avatar,
+            :notes,
+            :schedule_id,
+            :group_id))
+        respond_to do |format|
+            format.js { head :ok }
+            format.html { redirect_to @user }
+        end
+    end
 
-	def calculate_final_grade
-		# feature has to be enabled by supplying a grading.yml
-		raise ActionController::RoutingError.new('Not Found') if not Grading::FinalGradeAssigner.available?
-		@user = @user_scope.find(params[:id])
-		result = Grading::FinalGradeAssigner.assign_final_grade(@user, current_user, only: params[:grades])
-		redirect_to @user
-	end
+    def calculate_final_grade
+        # feature has to be enabled by supplying a grading.yml
+        raise ActionController::RoutingError.new('Not Found') if not Grading::FinalGradeAssigner.available?
+        @user = @user_scope.find(params[:id])
+        result = Grading::FinalGradeAssigner.assign_final_grade(@user, current_user, only: params[:grades])
+        redirect_to @user
+    end
 
-	private
+    private
 
-	# limits user operations to the scope allowed for the current user
-	def set_user_scope
-		@user_scope = case current_user.role
-		when 'assistant'
-			current_user.students
-		when 'head'
-			current_user.schedule.students
-		when 'admin'
-			User
-		end
-	end
+    # limits user operations to the scope allowed for the current user
+    def set_user_scope
+        @user_scope = case current_user.role
+        when 'assistant'
+            current_user.students
+        when 'head'
+            current_user.schedule.students
+        when 'admin'
+            User
+        end
+    end
 
 end
