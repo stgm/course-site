d291173ce68856ddf373563cb12b1a141a80c66b
Martijn Stegeman <m.stegeman@uva.nl>
2021-07-14 16:44:01 +0200

    Mailer Tuning (#404)
    * add warnings when there's no from address set, get started on html mails
    * only send feedback if there's actually feedback
    * add html template for grade mails and failed check mails
    * mention pset name in failed check mail
    * reset grade mailer frequency, only send grade mails after 1 hour

--- app/mailers/grade_mailer.rb
+++ app/mailers/grade_mailer.rb
@@ -1,26 +1,30 @@
 class GradeMailer < ApplicationMailer
 
-	helper GradesHelper
+    helper GradesHelper
 
-	def new_mail(grade)
-		@course_name = Course.short_name
-		@grade_name = grade.pset.name
-		@feedback = grade.comments
-		if Settings.grading && Settings.grading['grades'][grade.submit.pset.name] && !Settings.grading['grades'][grade.submit.pset.name]['hide_calculated']
-			@grade = grade.any_final_grade
-		else
-			@grade = grade.grade
-		end
-		@login = grade.submit.used_login if grade.submit
-		@header = File.read("#{Rails.root}/public/course/mail/grade.txt") if File.exists?("#{Rails.root}/public/course/mail/grade.txt")
-		mail(to: grade.user.mail, subject: "#{Course.short_name}: feedback for #{@grade_name}")
-	end
-	
-	def bad_submit(submit)
-		@course_name = Course.short_name
-		@grade_name = submit.pset_name
-		@login = submit.used_login
-		mail(to: submit.user.mail, subject: "#{@course_name}: warning about failed check")
-	end
-	
+    def new_mail(grade)
+        @course_name = Course.short_name
+        @grade_name = grade.pset.name
+        @feedback = grade.comments
+        if Settings.grading && Settings.grading['grades'][grade.submit.pset.name] && !Settings.grading['grades'][grade.submit.pset.name]['hide_calculated']
+            @grade = grade.any_final_grade
+        else
+            @grade = grade.grade
+        end
+        @login = grade.submit.used_login if grade.submit
+        @header = File.read("#{Rails.root}/public/course/mail/grade.txt") if File.exists?("#{Rails.root}/public/course/mail/grade.txt")
+        mail(to: grade.user.mail, subject: "#{Course.short_name}: feedback for #{@grade_name}")
+    end
+
+    def bad_submit(submit)
+        @course_name = Course.short_name
+        @grade_name = submit.pset_name
+        @login = submit.used_login
+        mail(to: submit.user.mail, subject: "#{@course_name}: failed check for #{@grade_name}")
+    end
+
+    def self.available?
+        Settings.send_grade_mails && super
+    end
+
 end
