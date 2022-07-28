62b0cb2e047d72db2c7ed4bd95d3589de9e26a60
Martijn Stegeman <martijn@stgm.nl>
2022-07-28 09:59:30 +0200

    first registration sequence

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,11 @@ class HomeController < ApplicationController
     def index
         if logged_in?
             if !current_user.valid_profile?
+                # require profile completion
                 redirect_to profile_path
+            elsif current_user.admin? && Settings.git_repo.blank?
+                # allow connecting course materials git
+                redirect_to welcome_clone_path
             elsif alerts_for_current_schedule.any?
                 # current user's schedule's announcements
                 redirect_to announcements_path
