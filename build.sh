fe146de4b26dd380622d9fd96f22af73f318dd25
Gerrit Oomens <g.oomens@uva.nl>
2022-08-12 14:28:31 +0200

    Add secrets and more parameters

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 set -euxo pipefail
 
-docker build . -t fnwicr.azurecr.io/minprog
+docker buildx build --platform linux/amd64 . -t fnwicr.azurecr.io/minprog
 docker push fnwicr.azurecr.io/minprog    
 
-VERSION="0.2.3"
+VERSION="0.2.6"
 NAME="minprog"
 helm package --version "${VERSION}" --app-version "${VERSION}" "charts/${NAME}"
-helm chart save "${NAME}-${VERSION}.tgz" "fnwicr.azurecr.io/helm/${NAME}:${VERSION}"
-helm chart push "fnwicr.azurecr.io/helm/${NAME}:${VERSION}"
+#helm chart save "${NAME}-${VERSION}.tgz" "fnwicr.azurecr.io/helm/${NAME}:${VERSION}"
+helm push "${NAME}-${VERSION}.tgz" oci://fnwicr.azurecr.io/helm
