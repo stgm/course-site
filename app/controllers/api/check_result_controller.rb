474213d1aed7e5810e2c47b8222a07ee9956b8b9
Martijn Stegeman <martijn@stgm.nl>
2022-09-24 07:37:14 +0200

    more diag

--- app/controllers/api/check_result_controller.rb
+++ app/controllers/api/check_result_controller.rb
@@ -6,10 +6,11 @@ class Api::CheckResultController < ApplicationController
 		submit = Submit.find_by_check_token(params["id"])
 		if submit
 			results = params["result"]
+            results2= results
 			# TODO insert validator
 			results.permit!
             # submit.register_auto_check_results(results.to_h)
-            submit.update!(check_results: [results, results.permit.to_h]
+            submit.update!(check_results: [results2, results]
 			head :ok
 		end
 	end
