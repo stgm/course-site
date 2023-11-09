53638cef6fa667cd26ec92579e9868e52e4fa3fc
Martijn Stegeman <martijn@stgm.nl>
2023-11-09 15:23:18 +0100

    grade config load

--- app/models/course/tools.rb
+++ app/models/course/tools.rb
@@ -26,7 +26,11 @@ class Course::Tools
 
                 # set a few flags from config for easier queries later on
                 p.automatic = p.config.present? && p.config["automatic"].present?
-                p.grade_type = definition['type'] || :float
+                # get potential type from final grade config
+                calc_type = GradingConfig.all.
+                            select { |k,v| v['submits'] && v['submits']['algorithms'] }.
+                            map{ |k,v| v['type'] }.compact&.first
+                p.grade_type = definition['type'] || calc_type || :float
                 p.test = definition['is_test'] || false
                 p.save
             end
