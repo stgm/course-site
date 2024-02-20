a155603c1951a35897a2220f34cb709739f189be
Martijn Stegeman <martijn@stgm.nl>
2024-02-20 11:14:00 +0100

    url

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -26,7 +26,7 @@ class ExamsController < ApplicationController
         if Rails.env.development?
             redirect_to "http://localhost:8009/?#{params}"
         else
-            redirect_to "https://uvapl.github.io/examide/?url=#{params}", allow_other_host: true
+            redirect_to "https://uvapl.github.io/examide/?#{params}", allow_other_host: true
         end
     end
 
