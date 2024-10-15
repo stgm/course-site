d31d4bed862c56d9931c0b9ae40c9a295bf7f410
Martijn Stegeman <martijn@stgm.nl>
2024-10-15 14:36:57 +0200

    why the subs

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -10,7 +10,7 @@ class ExamsController < ApplicationController
     def index
         # get all exams from config that may be submitted
         # allow student to choose one and start
-        @exams = Exam.joins(:pset).includes(pset: :submits).order(:name)
+        @exams = Exam.joins(:pset).order(:name)
         @exams = @exams.where(locked: false) if !current_user.admin?
         if Settings.registration_phase == 'exam'
             render layout: 'blank' and return
