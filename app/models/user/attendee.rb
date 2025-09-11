04a6629eb9e42e6ecad617340c436f4b5058228c
Martijn Stegeman <m.stegeman@uva.nl>
2025-09-11 13:49:59 +0200

    nooo

--- app/models/user/attendee.rb
+++ app/models/user/attendee.rb
@@ -34,7 +34,7 @@ module User::Attendee
 
             # update user properties
             props = { last_seen_at: now, location_confirmed: ar.confirmed }
-            props[:last_known_location] = nil if !same_ip_as_previous_hour(ar, prev)
+            props[:last_known_location] = nil if prev && !same_ip_as_previous_hour(ar, prev)
             update_columns(props)
             take_attendance
         end
