61b5308322b2336a8b7704399f0adf85cc6853a8
Martijn Stegeman <m.stegeman@uva.nl>
2025-10-31 08:39:15 +0100

    disable

--- app/settings/settings.rb
+++ app/settings/settings.rb
@@ -49,7 +49,7 @@ class Settings < RailsSettings::Base
     end
 
     scope :submit_system do
-        field :submit_disabled, default: false, readonly: true
+        field :submit_disabled, default: "It's not possible to submit assignments this weekend. Try again from Monday at 10:00.", readonly: true
         field :webdav_base
         field :webdav_user
         field :webdav_pass
