4dc974512cd7f05f24e6fa769e5f26b54d1aa8e9
Martijn Stegeman <m.stegeman@uva.nl>
2025-11-20 10:24:26 +0100

    fix clearing of attendance requests

--- app/models/attendance_record.rb
+++ app/models/attendance_record.rb
@@ -1,16 +1,15 @@
 class AttendanceRecord < ApplicationRecord
 
+    scope :users_with_recent_records, -> {
+        where(cutoff: [ previous_hour, current_hour ]).select(:user_id)
+    }
+
     def self.reset_stale_locations
         return if Settings.reset_stale_locations > 30.minutes.ago
         Settings.reset_stale_locations = Time.current
 
         current_hour = Time.current.beginning_of_hour
         previous_hour = current_hour - 1.hour
-
-        users_with_recent_records = AttendanceRecord
-          .where(cutoff: [ previous_hour, current_hour ])
-          .where(confirmed: true)
-          .select(:user_id)
 
         User
           .where.not(id: users_with_recent_records)
