d0d311005d86210d816047b4c647ba03853ef54d
Martijn Stegeman <m.stegeman@uva.nl>
2026-04-17 10:31:06 +0200

    pin auth message improvement

--- app/controllers/auth/pin_controller.rb
+++ app/controllers/auth/pin_controller.rb
@@ -1,6 +1,6 @@
 class Auth::PinController < ApplicationController
 
-    # Facilitates login by pin
+    # Facilitates login by pin, for exams
 
     def self.available?
         Settings.registration_phase == "exam"
@@ -13,23 +13,29 @@ class Auth::PinController < ApplicationController
     end
 
     def validate
-        if  Settings.registration_phase == "exam" &&
-            Settings.exam_code &&
-            params[:exam_code].size >= 5 &&
-            params[:pin_code].size == 4 &&
-            Settings.exam_code == params[:exam_code] &&
-            @user = User.authenticate_by_pin(params[:pin_code])
-                if @user.last_known_ip.blank?
-                    @user.update(last_known_ip: request.remote_ip)
-                elsif @user.last_known_ip != request.remote_ip
-                    redirect_to root_url, alert: "Wrong IP" and return
-                end
-                session[:user_id] = @user.id
-                session[:user_mail] = @user.mail
-                redirect_to exams_url
-        else
-            redirect_to root_url, alert: "Something was wrong"
+        unless Settings.registration_phase == "exam" && Settings.exam_code
+            redirect_to root_url, alert: "No current exam" and return
         end
+        unless  params[:exam_code].size >= 5 &&
+                params[:pin_code].size == 4 &&
+                Settings.exam_code == params[:exam_code] &&
+                @user = User.authenticate_by_pin(params[:pin_code])
+            redirect_to root_url, alert: "Invalid credentials" and return
+        end
+        unless @user.status_active?
+            redirect_to root_url, alert: "User marked inactive, ask teacher" and return
+        end
+        unless @user.can_submit?
+            redirect_to root_url, alert: "User has no student id to submit with" and return
+        end
+        if @user.last_known_ip.blank?
+            @user.update(last_known_ip: request.remote_ip)
+        elsif @user.last_known_ip != request.remote_ip
+            redirect_to root_url, alert: "Wrong IP" and return
+        end
+        session[:user_id] = @user.id
+        session[:user_mail] = @user.mail
+        redirect_to exams_url
     end
 
 end
