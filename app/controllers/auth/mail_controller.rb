cc33a4e46682baa42d53456ceb1d5669da504618
Martijn Stegeman <m.stegeman@uva.nl>
2026-04-21 11:27:20 +0200

    add code expiry

--- app/controllers/auth/mail_controller.rb
+++ app/controllers/auth/mail_controller.rb
@@ -36,6 +36,8 @@ class Auth::MailController < ApplicationController
         code = SecureRandom.hex(3)
         # hash it for later check
         session[:login_secret] = Digest::SHA256.hexdigest(code)
+        # record issue time so we can expire stale codes
+        session[:login_secret_at] = Time.now.to_i
         AuthMailer.with(mail: mail, code: code).login_code.deliver_later
         redirect_to auth_mail_code_path
     end
@@ -46,9 +48,17 @@ class Auth::MailController < ApplicationController
 
     def validate
         if params[:code].size == 6
+            # reject codes older than 15 minutes
+            if login_code_expired?
+                clear_login_session
+                redirect_to auth_mail_login_path, alert: "Code expired, please request a new one" and return
+            end
+
             if session[:login_secret] == Digest::SHA256.hexdigest(params[:code])
                 # retrieve previously entered e-mail address
                 mail = session[:login_email]
+                # always remove entered details before redirecting
+                clear_login_session
                 # find existing user or create new (if possible)
                 if @user = User.authenticate({ mail: mail })
                     session[:user_id] = @user.id
@@ -57,12 +67,26 @@ class Auth::MailController < ApplicationController
                 else
                     redirect_to root_url, alert: t("account.not_everyone_can_login")
                 end
+            else
+                # invalidate the code immediately on any wrong attempt
+                clear_login_session
+                redirect_to auth_mail_login_path, alert: "Invalid code, please request a new one"
             end
         else
+            clear_login_session
             redirect_to root_url
         end
-        # always remove entered details, whether successful or not
+    end
+
+    private
+
+    def login_code_expired?
+        session[:login_secret_at].blank? || (Time.now.to_i - session[:login_secret_at].to_i) > 15.minutes
+    end
+
+    def clear_login_session
         session.delete(:login_secret)
+        session.delete(:login_secret_at)
         session.delete(:login_email)
     end
 
