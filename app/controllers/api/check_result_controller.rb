7f047f5e6734ba5e398089e31dc0c2aed2a6ce8e
Martijn Stegeman <m.stegeman@uva.nl>
2026-05-08 14:48:43 +0200

    check result api improvements

--- app/controllers/api/check_result_controller.rb
+++ app/controllers/api/check_result_controller.rb
@@ -4,13 +4,19 @@ class Api::CheckResultController < ApplicationController
 
     def do
         submit = Submit.find_by_check_token(params["id"])
-        if submit
-            results = params["result"]
-            # TODO insert validator
-            results.permit!
-            submit.register_auto_check_results(results.to_h)
-            head :ok
+
+        unless submit
+            head :unauthorized and return
         end
+
+        results = params.require(:result).permit(
+            summary: [ :total_check_count, :passed_check_count ],
+            runs: [ :name, { results: [ :description, :log, :message, :passed ] } ],
+            error: [ :value ]
+        )
+
+        submit.register_auto_check_results(results.to_h)
+        head :ok
     end
 
 end
