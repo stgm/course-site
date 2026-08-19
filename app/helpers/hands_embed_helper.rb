250a2a7659c23045805f298faa0c9a5332762d19
Martijn Stegeman <martijn@stgm.nl>
2026-08-19 14:22:51 +0200

    final integration of hands

--- app/helpers/hands_embed_helper.rb
+++ app/helpers/hands_embed_helper.rb
@@ -4,9 +4,7 @@ module HandsEmbedHelper
     # only students actually sitting in the lab, never staff and never people
     # working remotely.
     def hands_embed_may_interrupt?
-        # keep this during pre-implementation
-        true
-        # is_local_ip? && current_user.student?
+        is_local_ip? && current_user.student?
     end
 
 end
